77d53c373dc1b25d04559993f6d6899601e2eca9
Hieu Ho <hihho@ucdavis.edu>
2020-04-06 12:50:29 -0700

    Added GroceryList as class, added bold on hover

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 // TODO
-let GroceryList = (props) => (
-  <div id='app'>
-    <ul>
-      <li>{props.list[0]}</li>
-      <li>{props.list[1]}</li>
-    </ul>
-  </div>
-);
+class GroceryListItems extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bold: false
+    }
+  }
+
+  // onListItemClick() {
+  //   this.setState ({
+  //     bought: !this.state.bought
+  //   });
+  // }
+
+  onListItemHover() {
+    this.setState ({
+      bold: !this.state.bold
+    })
+  }
+
+  render() {
+    let style = {
+      fontWeight: this.state.bold ? 'bolder' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)} onMouseOut={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+  var GroceryList = (props) => (
+      <ul>
+        {props.items.map( item =>
+          <GroceryListItems item = {item} />
+          )}
+      </ul>
+  );
 
 
 ReactDOM.render(
-  <GroceryList list={['orange', 'apple']}/>,
+  <GroceryList items={['orange', 'apple']}/>,
   document.getElementById('app')
 );
