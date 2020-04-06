994aa351671ef5cc0c05fc19c35977fdb7e93cd0
Hieu Ho <hihho@ucdavis.edu>
2020-04-06 12:04:36 -0700

    Implemented bare minimum req

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // TODO
+let GroceryList = (props) => (
+  <div id='app'>
+    <ul>
+      <li>{props.list[0]}</li>
+      <li>{props.list[1]}</li>
+    </ul>
+  </div>
+);
+
+
+ReactDOM.render(
+  <GroceryList list={['orange', 'apple']}/>,
+  document.getElementById('app')
+);
